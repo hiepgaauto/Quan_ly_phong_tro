8fad21ca03786590b9bdc099ff189dea6428fefe
nhhiep <nhhiep@cmc.com.vn>
2020-11-16 11:12:49 +0700

    pull all code

---
+++ Quan_ly_phong_tro/Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quan_ly_phong_tro.Model
+{
+    class Class1
+    {
+    }
+}
---
+++ Quan_ly_phong_tro/PageAddCustomer.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Quan_ly_phong_tro.Model;
+using SQLite;
+namespace Quan_ly_phong_tro
+{
+    /// <summary>
+    /// Interaction logic for PageAddCustomer.xaml
+    /// </summary>
+    public partial class PageAddCustomer : Page
+    {
+        public PageAddCustomer()
+        {
+            InitializeComponent();
+        }
+            private void checkedSexCustomer(object sender, RoutedEventArgs e)
+        {
+            // ... Get RadioButton reference.
+            var button = sender as RadioButton;
+
+            // ... Display button content as title.
+            this.Title = button.Content.ToString();
+        }
+        private void insertCustomer(object sender, RoutedEventArgs e)
+        {
+            var checkValue= checkGender.Children.OfType<RadioButton>()
+                 .FirstOrDefault(r => r.IsChecked.HasValue && r.IsChecked.Value);
+            string s = checkValue.IsChecked==true ? "male" : "female";
+            Customer customer = new Customer()
+            {
+                name = nameCustomer.Text,
+                address = address.Text,
+                birthYear = int.Parse(birth.Text),
+                jobCustomer = job.Text,
+                numberPhone1 = numphone1.Text,
+                numberPhone2 = numphone2.Text,
+                dateRent = dayRent.DisplayDate,
+                room = room.Text,
+                gender = s.ToString(),
+                cmnd = cmnd.Text,
+            };
+            SQLiteConnection con = new SQLiteConnection(App.connecter);
+            con.Insert(customer);
+            //con.Close;
+        }
+    }
+}
